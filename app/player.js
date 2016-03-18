441f84863bc99cd55edaaa3fb0b371c8c2062e76
Robin <robin@xerdi.com>
2016-03-18 10:29:46 +0100

    Added docstrings

--- app/player.js
+++ app/player.js
@@ -2,6 +2,24 @@
  * Created by Robin on 3/15/2016.
  */
 
+/**
+ *
+ * @param {string} name
+ * @param {string} side
+ * 
+ * @param {Object} position             Position of the player.
+ * @param {number} position.x
+ * @param {number} position.y
+ * 
+ * @param {Object} size                 Size of the player
+ * @param {number} size.width
+ * @param {number} size.height
+ *
+ * @param {string} shape
+ * @param {string} color                Color of the player, e.g. "#FF0000" or "red".
+ *
+ * @constructor
+ */
 var Player = function(name, side, position, size, shape, color) {
     this.gameID = null;
     this.name = name;
@@ -12,6 +30,11 @@ var Player = function(name, side, position, size, shape, color) {
     this.color = color;
 };
 
+/**
+ * Draws the player on the given canvas context.
+ *
+ * @param {Object} context
+ */
 Player.prototype.draw = function(context) {
     // TODO drawing logic here
     //console.log("Drawing player ", this.gameID, " width: ", this.size["width"]);
@@ -19,6 +42,12 @@ Player.prototype.draw = function(context) {
     context.fillRect(this.position.x, this.position.y, this.size.width, this.size.height);
 };
 
+/**
+ * Reviver function to create a new Player from the given Json.
+ * 
+ * @param {Object} json
+ * @returns {Player}
+ */
 Player.revive = function(json){
     var newPlayer = new Player(json.name, json.side, json.position, json.size, json.shape, json.color);
     newPlayer.gameID = json.gameID;
--- app/projectile.js
+++ app/projectile.js
@@ -2,22 +2,53 @@
  * Created by Robin on 3/16/2016.
  */
 
+/**
+ *
+ * @param {number} radius               The radius of the projectile.
+ *
+ * @param {Object} speed                The speed of the projectile.
+ * @param {number} speed.dx
+ * @param {number} speed.dy
+ *
+ * @param {Object} startPosition        Starting position.
+ * @param {number} startPosition.x
+ * @param {number} startPosition.y
+ *
+ * @constructor
+ */
 var Projectile = function(radius, speed, startPosition) {
     this.radius = radius;
     this.speed = speed;
     this.position = startPosition;
 };
 
+/**
+ * This method simply returns radius*2, you could use this to get the width or height
+ * of the projectile.
+ *
+ * @returns {number} Width or Height
+ */
 Projectile.prototype.getWidthHeight = function() {
     return this.radius*2;
 };
 
+/**
+ * Draws the projectile on the given canvas context.
+ *
+ * @param {Object} context              The context derived from the canvas.
+ */
 Projectile.prototype.draw = function(context) {
     //TODO drawing logic here
     context.fillStyle = "#000000";
     context.fillRect(this.position.x, this.position.y, this.radius*2, this.radius*2);
 };
 
+/**
+ * Reviver function to create a new Projectile from the given Json.
+ *
+ * @param {Object} json
+ * @returns {Projectile}
+ */
 Projectile.revive = function(json) {
     return new Projectile(json.radius, json.speed, json.position);
 };
