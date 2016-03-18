16271e435badb645796917523bc00274b074dd0d
SanderBouwman <Bouwman_8@hotmail.com>
2016-03-18 14:53:46 +0100

    Added proper collision

--- app/projectile.js
+++ app/projectile.js
@@ -38,8 +38,10 @@ Projectile.prototype.getWidthHeight = function () {
  */
 Projectile.prototype.draw = function (context) {
     //TODO drawing logic here
+    context.beginPath();
+    context.arc(this.position.x, this.position.y, this.radius, 2 * Math.PI, false);
     context.fillStyle = "#000000";
-    context.fillRect(this.position.x, this.position.y, this.radius * 2, this.radius * 2);
+    context.fill();
 };
 
 /**
@@ -52,52 +54,42 @@ Projectile.revive = function (json) {
     return new Projectile(json.radius, json.speed, json.position);
 };
 
+/**
+ * @param {Element} canvas
+ * @param {Object[]} players
+ * @author Sander
+ */
 Projectile.prototype.move = function (canvas, players) {
     this.position.x += this.speed.dx;
     this.position.y += this.speed.dy;
 
     if (this.position.y <= 0) {
         this.position.y = 0;
-        // leave current_y symmentrical
         this.position.y -= this.speed.dy;
         this.speed.dy *= -1;
     } else if (this.position.y >= canvas.height) {
         this.position.y = canvas.height;
-        // leave current_y symmentrical
         this.position.y -= this.getWidthHeight();
         this.speed.dy *= -1;
     }
 
-
-    if ((this.position.x >= (canvas.width - players[1].size.width + 5)) && (this.position.y >= players[1].position.y) && (this.position.y <= players[1].position.y + players[1].size.height)) {
+    if ((this.position.x + this.radius >= (canvas.width - players[1].size.width + 5)) && (this.position.y >= players[1].position.y) && (this.position.y <= players[1].position.y + players[1].size.height)) {
         this.position.x = (canvas.width - players[1].size.width - this.speed.dx);
+//        this.speed.dy += players[1].speed / 20;
         this.speed.dx *= -1;
-        this.ballDirection(players[1]);
-    } else if ((this.position.x <= (players[0].size.width - 5)) && (this.position.y >= players[0].position.y) && (this.position.y <= (players[0].position.y + players[0].size.height))) {
+        this.position.x += this.speed.dx;
+    } else if ((this.position.x - this.radius <= (players[0].size.width - 5)) && (this.position.y >= players[0].position.y) && (this.position.y <= (players[0].position.y + players[0].size.height))) {
         this.position.x = (players[0].size.width) - this.speed.dx;
+//        this.speed.dy += players[0].speed / 2;
         this.speed.dx *= -1;
-        this.ballDirection(players[0]);
+        this.position.x += this.speed.dx;
     }
 
     this.draw(canvas.getContext("2d"));
-//    if (this.position.x > canvas.width || this.position.x < 0) {
+    if (this.position.x > canvas.width || this.position.x < 0) {
 //        var player_won = (this.position.x > canvas.width) ? 1 : 2;
 //        window.player_id_having_the_ball = (player_won == 1) ? 2 : 1;
 //        finish_round(player_won);
 //        socket.json.emit("end_of_the_round", {player_won: player_won, room_id: window.room_id});
-//    }
-
-};
-
-Projectile.prototype.ballDirection = function (player) {
-    if (this.position.y < player.position.y + player.size.height / 2) {
-        if (this.speed.dy > 0) {
-            this.speed.dy *= -1;
-        }
-    } else if (this.position.y > player.position.y + player.size.height / 2) {
-        if (this.speed.dy < 0) {
-            this.speed.dy *= -1;
-        }
-
     }
 };
