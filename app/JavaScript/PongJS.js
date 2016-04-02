95ee0fc3081c6fe1abf6ec09ecb280494694573d
SanderBouwman <Bouwman_8@hotmail.com>
2016-04-02 12:37:58 +0200

    Bug fix

--- app/JavaScript/PongJS.js
+++ app/JavaScript/PongJS.js
@@ -49,7 +49,7 @@ clearBody();
 }
 } else if (gamestate === "menu") {
 
-if (e.keyCode === 13) {//if pressed key = enter  
+if (e.keyCode === 13) {//if pressed key = enter
 $(".selected").each(function () {
 var id = $(this).attr("id");
         if (id === "play") {
@@ -97,7 +97,182 @@ $("#testtabel").addClass("selected");
 //
 // IN GAME JAVASCRIPT
 function play(){
-var scoreboard = document.getElementsByClassName("score-board")[0],
+
+var Player = function (position, size, color, speed) {
+this.id = null;
+        this.size = size;
+        this.position = position;
+        this.color = color;
+        this.speed = speed;
+        this.score = 0;
+        this.ready = false;
+        };
+        /**
+         * Draws the player on the given canvas context.
+         *
+         * @param {Object} context
+         */
+        Player.prototype.draw = function (context) {
+        context.fillStyle = this.color;
+                context.fillRect(this.position.x, this.position.y, this.size.width, this.size.height);
+        };
+        /**
+         * Reviver function to create a new Player from the given Json.
+         *
+         * @param {Object} json
+         * @returns {Player}
+         */
+        Player.revive = function (json) {
+        var newPlayer = new Player(json.position, json.size, json.color, json.speed);
+                newPlayer.id = json.id;
+                return newPlayer;
+        };
+        /**
+         * @author Sander
+         * @param {number} y
+         * @param {Object} canvas
+         */
+        Player.prototype.move = function (y, canvas) {
+        this.position.y += y;
+                this.speed = y;
+                if (this.position.y < 0) {
+        this.position.y = 0;
+                this.speed = 0;
+        } else if (this.position.y + this.size.height > canvas.height) {
+        this.position.y = canvas.height - this.size.height;
+                this.speed = 0;
+        }
+        };
+        var keysDown = {};
+        window.addEventListener("keydown", function (event) {
+        keysDown[event.keyCode] = true;
+        });
+        window.addEventListener("keyup", function (event) {
+        delete keysDown[event.keyCode];
+        });
+        /**
+         * @author Sander
+         */
+        Player.prototype.update = function (canvas, room, socket) {
+        var oldY = this.position.y;
+                var oldReady = this.ready;
+//Keycode 40 = the arrowdown key, if it is pressed the player, obviously, will move down.
+                //There is also a check to prevent the player from leaving the playing field.
+                // console.log("Update... Keys -> ", keysDown);
+                // console.log("Y: ", this.position.y);
+                for (var key in keysDown) {
+        var value = Number(key);
+                if (value === 40) {
+        this.move(7, canvas);
+                //Keycode 38 = the arrowup key, if it is pressed the player, obviously, will move up.
+        } else if (value === 38) {
+        this.move( - 7, canvas);
+        } else if (value === 32) {
+        this.ready = true;
+        } else {
+        this.move(0, canvas);
+        }
+        }
+
+        // console.log(oldReady, this.ready);
+        if (this.position.y !== oldY || this.ready !== oldReady) {
+        socket.emit("playerupdate", {"roomNumber": room, "player": this});
+        }
+        };
+        var Projectile = function (radius, speed, startPosition) {
+        this.radius = radius;
+                this.speed = speed;
+                this.position = startPosition;
+                this.gameOver = false;
+        };
+        Projectile.prototype.update = function (canvas, players, room, socket) {
+        var oldX = this.position.x,
+                oldY = this.position.y;
+                this.move(canvas, players);
+                if ((this.position.x !== oldX || this.position.y !== oldY) || this.gameOver) {
+        this.gameOver = false;
+                socket.emit("ballupdate", {"roomNumber": room, "projectile": this});
+        }
+
+        };
+        /**
+         * This method simply returns radius*2, you could use this to get the width or height
+         * of the projectile.
+         *
+         * @returns {number} Width or Height
+         */
+        Projectile.prototype.getWidthHeight = function () {
+        return this.radius * 2;
+        };
+        /**
+         * Draws the projectile on the given canvas context.
+         *
+         * @param {Object} context              The context derived from the canvas.
+         */
+        Projectile.prototype.draw = function (context) {
+        //TODO drawing logic here
+        context.beginPath();
+                context.arc(this.position.x, this.position.y, this.radius, 2 * Math.PI, false);
+                context.fillStyle = "#000000";
+                context.fill();
+        };
+        /**
+         * Reviver function to create a new Projectile from the given Json.
+         *
+         * @param {Object} json
+         * @returns {Projectile}
+         */
+        Projectile.revive = function (json) {
+        return new Projectile(json.radius, json.speed, json.position);
+        };
+        /**
+         * @param {Element} canvas
+         * @param {Object[]} players
+         * @author Sander
+         */
+        Projectile.prototype.move = function (canvas, players) {
+        this.position.x += this.speed.dx;
+                this.position.y += this.speed.dy;
+                if (this.position.y <= 0) {
+        this.position.y = 0;
+                this.position.y -= this.speed.dy;
+                this.speed.dy *= - 1;
+        } else if (this.position.y >= canvas.height) {
+        this.position.y = canvas.height;
+                this.position.y -= this.getWidthHeight();
+                this.speed.dy *= - 1;
+        }
+
+        if ((this.position.x + this.getWidthHeight() >= (canvas.width - players[1].size.width + 5)) && (this.position.y >= players[1].position.y) && (this.position.y <= players[1].position.y + players[1].size.height)) {
+        this.position.x = (canvas.width - players[1].size.width * 2 - this.speed.dx);
+                // this.speed.dy += (players[1].speed / 2);
+                this.speed.dx *= - 1;
+                this.position.x += this.speed.dx;
+        } else if ((this.position.x - this.getWidthHeight() <= (players[0].size.width - 5)) && (this.position.y >= players[0].position.y) && (this.position.y <= (players[0].position.y + players[0].size.height))) {
+        this.position.x = (players[0].size.width * 2) - this.speed.dx;
+                // this.speed.dy += (players[0].speed / 2);
+                this.speed.dx *= - 1;
+                this.position.x += this.speed.dx;
+        }
+
+        // this.draw(canvas.getContext("2d"));
+        if (this.position.x > canvas.width || this.position.x < 0) {
+        if (this.position.x > canvas.width)
+                players[0].score++;
+                if (this.position.x < 0)
+                players[1].score++;
+                this.finishRound(canvas, players);
+        }
+        };
+        Projectile.prototype.finishRound = function (canvas, players) {
+        for (var i in players) {
+        players[i].ready = false;
+                this.position.x = canvas.width / 2;
+                this.position.y = canvas.height / 2;
+        }
+        this.gameOver = true;
+        };
+        var scoreboard = document.getElementsByClassName("score-board")[0],
         playerScores = scoreboard.getElementsByClassName("player"),
         player1Score = playerScores[0].getElementsByTagName('p')[0],
         player2Score = playerScores[1].getElementsByTagName('p')[0];
@@ -279,179 +454,5 @@ if (socket !== undefined) {
 socket.close();
 }
 });
-        var Player = function (position, size, color, speed) {
-        this.id = null;
-                this.size = size;
-                this.position = position;
-                this.color = color;
-                this.speed = speed;
-                this.score = 0;
-                this.ready = false;
-        };
-        /**
-         * Draws the player on the given canvas context.
-         *
-         * @param {Object} context
-         */
-        Player.prototype.draw = function (context) {
-        context.fillStyle = this.color;
-                context.fillRect(this.position.x, this.position.y, this.size.width, this.size.height);
-        };
-        /**
-         * Reviver function to create a new Player from the given Json.
-         *
-         * @param {Object} json
-         * @returns {Player}
-         */
-        Player.revive = function (json) {
-        var newPlayer = new Player(json.position, json.size, json.color, json.speed);
-                newPlayer.id = json.id;
-                return newPlayer;
-        };
-        /**
-         * @author Sander
-         * @param {number} y
-         * @param {Object} canvas
-         */
-        Player.prototype.move = function (y, canvas) {
-        this.position.y += y;
-                this.speed = y;
-                if (this.position.y < 0) {
-        this.position.y = 0;
-                this.speed = 0;
-        } else if (this.position.y + this.size.height > canvas.height) {
-        this.position.y = canvas.height - this.size.height;
-                this.speed = 0;
-        }
-        };
-        var keysDown = {};
-        window.addEventListener("keydown", function (event) {
-        keysDown[event.keyCode] = true;
-        });
-        window.addEventListener("keyup", function (event) {
-        delete keysDown[event.keyCode];
-        });
-        /**
-         * @author Sander
-         */
-        Player.prototype.update = function (canvas, room, socket) {
-        var oldY = this.position.y;
-                var oldReady = this.ready;
-//Keycode 40 = the arrowdown key, if it is pressed the player, obviously, will move down.
-                //There is also a check to prevent the player from leaving the playing field.
-                // console.log("Update... Keys -> ", keysDown);
-                // console.log("Y: ", this.position.y);
-                for (var key in keysDown) {
-        var value = Number(key);
-                if (value === 40) {
-        this.move(7, canvas);
-                //Keycode 38 = the arrowup key, if it is pressed the player, obviously, will move up.
-        } else if (value === 38) {
-        this.move( - 7, canvas);
-        } else if (value === 32) {
-        this.ready = true;
-        } else {
-        this.move(0, canvas);
-        }
-        }
-
-        // console.log(oldReady, this.ready);
-        if (this.position.y !== oldY || this.ready !== oldReady) {
-        socket.emit("playerupdate", {"roomNumber": room, "player": this});
-        }
-        };
-        var Projectile = function (radius, speed, startPosition) {
-        this.radius = radius;
-                this.speed = speed;
-                this.position = startPosition;
-                this.gameOver = false;
-        };
-        Projectile.prototype.update = function (canvas, players, room, socket) {
-        var oldX = this.position.x,
-                oldY = this.position.y;
-                this.move(canvas, players);
-                if ((this.position.x !== oldX || this.position.y !== oldY) || this.gameOver) {
-        this.gameOver = false;
-                socket.emit("ballupdate", {"roomNumber": room, "projectile": this});
-        }
-
-        };
-        /**
-         * This method simply returns radius*2, you could use this to get the width or height
-         * of the projectile.
-         *
-         * @returns {number} Width or Height
-         */
-        Projectile.prototype.getWidthHeight = function () {
-        return this.radius * 2;
-        };
-        /**
-         * Draws the projectile on the given canvas context.
-         *
-         * @param {Object} context              The context derived from the canvas.
-         */
-        Projectile.prototype.draw = function (context) {
-        //TODO drawing logic here
-        context.beginPath();
-                context.arc(this.position.x, this.position.y, this.radius, 2 * Math.PI, false);
-                context.fillStyle = "#000000";
-                context.fill();
-        };
-        /**
-         * Reviver function to create a new Projectile from the given Json.
-         *
-         * @param {Object} json
-         * @returns {Projectile}
-         */
-        Projectile.revive = function (json) {
-        return new Projectile(json.radius, json.speed, json.position);
-        };
-        /**
-         * @param {Element} canvas
-         * @param {Object[]} players
-         * @author Sander
-         */
-        Projectile.prototype.move = function (canvas, players) {
-        this.position.x += this.speed.dx;
-                this.position.y += this.speed.dy;
-                if (this.position.y <= 0) {
-        this.position.y = 0;
-                this.position.y -= this.speed.dy;
-                this.speed.dy *= - 1;
-        } else if (this.position.y >= canvas.height) {
-        this.position.y = canvas.height;
-                this.position.y -= this.getWidthHeight();
-                this.speed.dy *= - 1;
-        }
-
-        if ((this.position.x + this.getWidthHeight() >= (canvas.width - players[1].size.width + 5)) && (this.position.y >= players[1].position.y) && (this.position.y <= players[1].position.y + players[1].size.height)) {
-        this.position.x = (canvas.width - players[1].size.width * 2 - this.speed.dx);
-                // this.speed.dy += (players[1].speed / 2);
-                this.speed.dx *= - 1;
-                this.position.x += this.speed.dx;
-        } else if ((this.position.x - this.getWidthHeight() <= (players[0].size.width - 5)) && (this.position.y >= players[0].position.y) && (this.position.y <= (players[0].position.y + players[0].size.height))) {
-        this.position.x = (players[0].size.width * 2) - this.speed.dx;
-                // this.speed.dy += (players[0].speed / 2);
-                this.speed.dx *= - 1;
-                this.position.x += this.speed.dx;
-        }
-
-        // this.draw(canvas.getContext("2d"));
-        if (this.position.x > canvas.width || this.position.x < 0) {
-        if (this.position.x > canvas.width)
-                players[0].score++;
-                if (this.position.x < 0)
-                players[1].score++;
-                this.finishRound(canvas, players);
-        }
-        };
-        Projectile.prototype.finishRound = function (canvas, players) {
-        for (var i in players) {
-        players[i].ready = false;
-                this.position.x = canvas.width / 2;
-                this.position.y = canvas.height / 2;
-        }
-        this.gameOver = true;
-        };
         }
 
